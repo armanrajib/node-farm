e118531af80daf63baee322b85556b31045c5289
Arman Rajib <ahrajib6789@gmail.com>
2024-12-15 16:11:05 +0600

    Build a very simple api

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+import fs from "fs";
 import http from "http";
 
+const data = fs.readFileSync("./dev-data/data.json", "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
-  console.log(req.url);
   const pathName = req.url;
 
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
